0f7ba3c61dc0491da6a09079c7571af2ce3a873c
mikupls <93015331+mikupls@users.noreply.github.com>
2021-11-15 03:39:33 +0100

    Add "open in reddit" button to all pages (#304)
    * Pass the url parameter to all templates. Add a reddit_link to the navbar, which opens the current url on reddit.
    
    * Add icon for reddit link
    
    Co-authored-by: spikecodes <19519553+spikecodes@users.noreply.github.com>

--- src/settings.rs
+++ src/settings.rs
@@ -14,6 +14,7 @@ use time::{Duration, OffsetDateTime};
 #[template(path = "settings.html")]
 struct SettingsTemplate {
 	prefs: Preferences,
+	url: String,
 }
 
 // CONSTANTS
@@ -35,7 +36,11 @@ const PREFS: [&str; 10] = [
 
 // Retrieve cookies from request "Cookie" header
 pub async fn get(req: Request<Body>) -> Result<Response<Body>, String> {
-	template(SettingsTemplate { prefs: Preferences::new(req) })
+	let url = req.uri().to_string();
+	template(SettingsTemplate {
+		prefs: Preferences::new(req),
+		url: url,
+	})
 }
 
 // Set cookies using response "Set-Cookie" header
