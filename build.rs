05c3cca365735f055ee0c7582335f065324b4324
Matthew Esposito <matt@matthew.science>
2023-01-04 16:31:27 -0500

    Non-git fallback

--- build.rs
+++ build.rs
@@ -1,6 +1,6 @@
 use std::process::Command;
 fn main() {
-	let output = Command::new("git").args(["rev-parse", "HEAD"]).output().unwrap();
-	let git_hash = String::from_utf8(output.stdout).unwrap();
+	let output = String::from_utf8(Command::new("git").args(["rev-parse", "HEAD"]).output().unwrap().stdout).unwrap_or_default();
+	let git_hash = if output == String::default() { "dev".into() } else { output };
 	println!("cargo:rustc-env=GIT_HASH={git_hash}");
 }
